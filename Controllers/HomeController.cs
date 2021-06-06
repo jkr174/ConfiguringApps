b3e8414c0dea9b4785fb87fbefba216953e66497
jkr174 <romo17410@gmail.com>
2021-06-06 13:05:26 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ConfiguringApps.Infrastructure;
+namespace ConfiguringApps.Controllers
+{
+    public class HomeController : Controller
+    {
+        private UptimeService uptime;
+        public HomeController(UptimeService up) => uptime = up;
+        public ViewResult Index()
+            => View(new Dictionary<string, string>
+            {
+                ["Message"] = "This is the Index action",
+                ["Uptime"] = $"{uptime.Uptime}ms"
+            });
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using ConfiguringApps.Infrastructure;
+
+namespace ConfiguringApps
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<UptimeService>();
+            services.AddMvc();
+        }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseMiddleware<ErrorMiddleware>();
+            app.UseMiddleware<BrowserTypeMiddleware>();
+            app.UseMiddleware<ShortCircuitMiddleware>();
+            app.UseMiddleware<ContentMiddleware>();
+        }
+    }
+}
